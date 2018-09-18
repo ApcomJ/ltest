adcc055e0b273ec1d4a7f59333b649f84f0af084
lufy <649217557@qq.com>
2018-09-18 10:30:26 +0800

    test git spring demo

---
+++ src/main/java/com/cqprosper/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.cqprosper.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** 
+ * @Description: 
+ * @Date: 2018/9/10 16:34 
+ * @Version V1.1
+ * @author: lufy
+ */  
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
