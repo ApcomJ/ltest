adff3e7aa5387baabdd15afaca8ddec290352184
ApcomJ <43233884+ApcomJ@users.noreply.github.com>
2018-09-18 11:11:47 +0800

    Update DemoApplication.java

--- src/main/java/com/cqprosper/demo/DemoApplication.java
+++ src/main/java/com/cqprosper/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /** 
- * @Description: 初始化方法
+ * @Description: 初始化方法,没错，测试更新
  * @Date: 2018/9/10 16:34 
  * @Version V1.1
  * @author: lufy
